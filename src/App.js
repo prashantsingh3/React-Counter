8e50dc3bc62560d0bf80250b7b2fd5e487718a11
prashantsingh3 <codewith1prashant@gmail.com>
2022-05-28 02:58:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+
+  
+
+  
+const App = () => {
+  
+  const [counter, setCounter] = useState(0)
+  
+
+  const increment = () => {
+   setCounter(counter + 2)
+  }
+  
+ 
+  const decrement = () => {
+    setCounter(counter - 1)
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div style={{
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      fontSize: '300%',
+      position: 'absolute',
+      width: '100%',
+      height: '100%',
+      top: '-15%',
+    }}>
+     
+      <div style={{
+        fontSize: '120%',
+        position: 'relative',
+        top: '10vh',
+      }}>
+        Counter: {counter}
+      </div>
+      <div className="buttons">
+        <button style={{
+          fontSize: '60%',
+          position: 'relative',
+          top: '20vh',
+          marginRight: '5px',
+          backgroundColor: 'green',
+          borderRadius: '8%',
+          color: 'white',
+        }}
+          onClick={increment}>Increment</button>
+        
 
+        <button style={{
+          fontSize: '60%',
+          position: 'relative',
+          top: '20vh',
+          marginLeft: '5px',
+          backgroundColor: 'red',
+          borderRadius: '8%',
+          color: 'white',
+        }}
+          onClick={decrement}>Decrement</button>
+      </div>
+    </div>
+  )
+}
+  
 export default App;
